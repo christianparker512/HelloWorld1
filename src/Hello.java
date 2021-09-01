487bbbdde6e227b51a35c4475583d471017eaadc
Christian Parker <christianparker512@gmail.com>
2021-09-01 09:33:05 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+
+//public is an access modifier that allows us to define the scope or how other parts of the code or even some one else's code can access this code.
+public class Hello {
+
+//    public is an access modifier
+    //need static for java to find our method to run the code we are going to add
+    //void indicates the method won't return anything.
+    //String[]args parameter
+    //curly braces is the block of code statements describing a method.
+    public static void main (String[]args){
+        //statement to be executed that includes expressions.
+        System.out.println("Hello Chris");
+    }
+}
+
